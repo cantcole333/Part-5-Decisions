030f21191e974ff974a9ac5d9ef0965c4a794221
Cole Cantwell <CantCole333@GoTVDSB.ca>
2020-09-29 10:13:07 -0400

    Finished part a Stages

--- Part 5 Decisons/Program.cs
+++ Part 5 Decisons/Program.cs
@@ -10,6 +10,45 @@ namespace Part_5_Decisons
     {
         static void Main(string[] args)
         {
+            int age;
+            Console.WriteLine("Please enter your age");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age <= 0)
+            {
+                Console.WriteLine("How are you typing if you're not born yet?");
+                Console.ReadLine();
+            }
+            else if (age <= 5)
+            {
+                Console.WriteLine("You are a toddler");
+                Console.ReadLine();
+            }
+
+            else if (age <= 10)
+            {
+                Console.WriteLine("You are a child");
+                Console.ReadLine();
+            }
+            else if (age <= 12)
+            {
+                Console.WriteLine("You are a preteen");
+                Console.ReadLine();
+            }
+            else if (age < 18)
+            {
+                Console.WriteLine("You are a teen");
+                Console.ReadLine();
+            }
+            else if (age <= 65)
+            {
+                Console.WriteLine("You are an adult");
+                Console.ReadLine();
+            }
+            else if (age >= 65)
+            {
+                Console.WriteLine("Enjoy your seniors discount!");
+                Console.ReadLine();
+            }
 
         }
     }
