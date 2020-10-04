ec28656a43f5e4c8f8bb5b8757fe84aa1293822b
Cole Cantwell <cole>
2020-10-04 17:53:54 -0400

    finished part 5

--- Part 5 Decisons/Program.cs
+++ Part 5 Decisons/Program.cs
@@ -10,45 +10,45 @@ namespace Part_5_Decisons
     {
         static void Main(string[] args)
         {
-            //int age;
-            //Console.WriteLine("Please enter your age");
-            //age = Convert.ToInt32(Console.ReadLine());
-            //if (age <= 0)
-            //{
-            //    Console.WriteLine("How are you typing if you're not born yet?");
-            //    Console.ReadLine();
-            //}
-            //else if (age <= 5)
-            //{
-            //    Console.WriteLine("You are a toddler");
-            //    Console.ReadLine();
-            //}
+            int age;
+            Console.WriteLine("Please enter your age");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age <= 0)
+            {
+                Console.WriteLine("How are you typing if you're not born yet?");
+                Console.ReadLine();
+            }
+            else if (age <= 5)
+            {
+                Console.WriteLine("You are a toddler");
+                Console.ReadLine();
+            }
 
-            //else if (age <= 10)
-            //{
-            //    Console.WriteLine("You are a child");
-            //    Console.ReadLine();
-            //}
-            //else if (age <= 12)
-            //{
-            //    Console.WriteLine("You are a preteen");
-            //    Console.ReadLine();
-            //}
-            //else if (age < 18)
-            //{
-            //    Console.WriteLine("You are a teen");
-            //    Console.ReadLine();
-            //}
-            //else if (age <= 65)
-            //{
-            //    Console.WriteLine("You are an adult");
-            //    Console.ReadLine();
-            //}
-            //else if (age >= 65)
-            //{
-            //    Console.WriteLine("Enjoy your seniors discount!");
-            //    Console.ReadLine();
-            //}
+            else if (age <= 10)
+            {
+                Console.WriteLine("You are a child");
+                Console.ReadLine();
+            }
+            else if (age <= 12)
+            {
+                Console.WriteLine("You are a preteen");
+                Console.ReadLine();
+            }
+            else if (age < 18)
+            {
+                Console.WriteLine("You are a teen");
+                Console.ReadLine();
+            }
+            else if (age <= 65)
+            {
+                Console.WriteLine("You are an adult");
+                Console.ReadLine();
+            }
+            else if (age >= 65)
+            {
+                Console.WriteLine("Enjoy your seniors discount!");
+                Console.ReadLine();
+            }
             Console.WriteLine("Enter a hurricane category that you'd like to learn about");
             int caseSwitch;
             Int32.TryParse(Console.ReadLine(), out caseSwitch);
@@ -73,6 +73,23 @@ namespace Part_5_Decisons
                     Console.WriteLine("That is not a hurricane category");
                     break;
             }
+
+            Random generator = new Random();
+            int compNum = generator.Next(2, 7);
+            int userNum;
+            int difference;
+            Console.WriteLine($"Please type a number larger than: {compNum}");
+            userNum = Convert.ToInt32(Console.ReadLine());
+            difference = userNum % compNum;
+
+            if (difference > 1)
+                Console.WriteLine("That is not divisible");
+            else
+            {
+                difference = 0;
+                Console.WriteLine("That is divisible");
+            }
+
             Console.ReadLine();
 
         }
